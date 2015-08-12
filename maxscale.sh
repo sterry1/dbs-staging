cbf3f3688af5d8c378a2c672953051c4dd954022
Steve Terry <sterry1@tacc.utexas.edu>
2015-08-12 19:23:03 +0000

    script to add ips to maxscale conf file

--- maxscale.sh
+++ maxscale.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 name="galera"
+rm maxscale.cnf
+cp pxc-MaxScale.cnf maxscale.cnf
 
 hosts=()
 ids=()
@@ -10,9 +12,9 @@ for ((i=1; i<=cnt; i++)); do
     container_id=$(docker ps | grep "$name-$i" | awk '{print $1}')
     echo "container id : $container_id"
     ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress  }}' $container_id)
-    echo "container ip : $ip" 
-    echo "docker exec $c \"sed -i \"s|.*wsrep_cluster_address.*=.*|$address|g\" $mycnf\""
-    docker exec $c sed -i "s|.*wsrep_cluster_address.*=.*|$address|g" $mycnf
-    hosts+=($ip)
-    ids+=($container_id)
+    echo "container ip : $ip"
+    #echo "docker exec $c \"sed -i \"s|.*wsrep_cluster_address.*=.*|$address|g\" $mycnf\""
+    sed -i  "s|{{dbserv$i}}|$ip|g"  maxscale.cnf
+    #hosts+=($ip)
+    #ids+=($container_id)
 done
