50f9e86312e8018e1db0b691235b63997dc8c4b8
Steve Terry <sterry1@tacc.utexas.edu>
2015-08-26 16:05:50 +0000

    added a cluster my.cnf file to hold the values we need for clustering
    renamed the compose file for maxscale to be more germane
    
    added the initial state my.cnf
    
    modified init.sh to fix user creation
    
    new yml file for percona cluster
    
    server.sh startup file modified for new volume mappings

--- server.sh
+++ server.sh
@@ -9,5 +9,5 @@ container_datadir=/var/lib/mysql
 host_datadir=/mnt/data/ubuntu-pxc56/mysql
 
 [[ ! -d $host_datadir ]] && sudo mkdir -p $host_datadir
-docker run -d --name="$container" -v $host_datadir:$container_datadir -v my.cnf:/tmp/my.cnf $image
+docker run -d --name="$container" -v $host_datadir:$container_datadir -v $(pwd)/my.cnf:/etc/mysql/my.cnf -v $(pwd)/trunc-core-data-7.23.sql:/var/lib/mysql/core-data.sql $image
 
