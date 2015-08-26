228fe35674f8c7e2573e9795b80777f6664f9527
Steve Terry <sterry1@tacc.utexas.edu>
2015-08-26 17:35:20 +0000

    added my.cnf to ignore
    added the my.cnf swaps to clean start
    
    removed my.cnf

--- clean-start.sh
+++ clean-start.sh
@@ -2,11 +2,17 @@
 
 echo "*** Starting cleanup for  fresh start...."
 echo ""
+echo "initalize the my.cnf file..."
+rm my.cnf
+cp init.my.cnf my.cnf
+echo ""
 echo "*** Stop and remove containers"
-docker rm `docker stop $(docker ps -aq)`
+# docker rm `docker stop $(docker ps -aq)`
+docker stop galera
+docker rm galera
 echo "*** containers removed"
 docker ps -a
 echo "*** remove mysql directory"
 sudo rm -rf /mnt/data/ubuntu-pxc56/mysql/
 echo "*** removed mysql directory" 
-ls /mnt/data/ubuntu-pxc56/mysql/
+ls -la /mnt/data/ubuntu-pxc56/mysql/
