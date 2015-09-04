dc3c3d5d82304cc71a2e54e8d47f27dc2df4ca6e
Steve Terry <sterry1@tacc.utexas.edu>
2015-09-04 09:23:58 -0500

    added the clean file

--- bin/server.sh
+++ bin/server.sh
@@ -3,11 +3,13 @@
 # adds the volume mapping for mysql /var/lib/mysql directory
 # as well as the my.cnf file with the cluster settings to be swapped out for /etc/mysql/my.cnf
 # with bootstrap startup
+type="core"
+num=1
 image="agaveapi/ubuntu:pxc56"
 container="galera"
 container_datadir=/var/lib/mysql
-host_datadir=/mnt/data/ubuntu-pxc56/mysql
+host_datadir=/mnt/data/ubuntu-pxc56/mysql-$type
 
 [[ ! -d $host_datadir ]] && sudo mkdir -p $host_datadir
-docker run -d --name="$container" -v $host_datadir:$container_datadir -v $(pwd)/trunc-core-data-7.23.sql:/tmp/core-data.sql  -v $(pwd)/my.cnf:/etc/mysql/my.cnf -p 3306:3306 -p 4444:4444 -p 4567:4567 $image
+docker run -d --name="$container-$type-$num" -v $host_datadir:$container_datadir -v $(pwd)/sql/trunc-core-data-7.23.sql:/tmp/core-data.sql  -v $(pwd)/percona/my.cnf:/etc/mysql/my.cnf -p 3301:3306 -p 3302:4444 -p 3303:4567 $image
 
